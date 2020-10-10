c307f1cb2958ee3b5f313de2333539f11e24beb9
Samvel <39568849+ssamvelk@users.noreply.github.com>
2020-10-10 23:22:45 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+  const s = [...str];
+
+  let i = 0;
+
+  while (i < s.length) {
+
+    bracketsConfig.forEach(el => {
+      if (s[i] === el[0] && s[i + 1] === el[1]) {
+        s.splice(i, 2);
+        if (i >= 0) i -= 2;
+      };
+    });
+
+    i += 1;
+  };
+
+  return s.length === 0 ? true : false;
 }
